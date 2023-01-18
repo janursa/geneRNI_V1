db6308c5b64a0f0ce4d4aacc2a6414d921441b64
Nourisa <jalil.nourisa@gmail.com>
2023-01-18 10:18:42 +0100

    major update on search functions
    replication is added to the search method. First, identical runs are executed for n iterations and stored in files. Then, the files are collected and the data are pooled to get best param and best score. Only rand_search is retained and other methods are removed,

--- geneRNI/search_param.py
+++ geneRNI/search_param.py
@@ -20,7 +20,7 @@ from .geneRNI import GeneEstimator
 from .data import Data
 
 
-def evaluate_single(X: np.ndarray, y: np.ndarray, param: dict, cv: int = 4, train_flag=False, **specs) -> Tuple[GeneEstimator, float]:
+def evaluate_single(X: np.ndarray, y: np.ndarray, param: dict, cv: int = 5, train_flag=False, **specs) -> Tuple[GeneEstimator, float]:
     """ evalutes the gene estimator for a given param and returns a test score 
     for RF, the test score is oob_score. For the rest, cv is applied.
     train_flag -- to use training score 
@@ -33,51 +33,42 @@ def evaluate_single(X: np.ndarray, y: np.ndarray, param: dict, cv: int = 4, trai
     est = GeneEstimator(**param)
     if use_oob_flag:
         est.fit(X, y)
-        train_score = est.score(X,y)
         test_score = est.est.oob_score_
     else:
-        cv_a = model_selection.ShuffleSplit(n_splits=cv, test_size=(1/cv)) 
-        test_scores = model_selection.cross_val_score(est, X, y, cv=cv_a)
-        oo = model_selection.cross_validate(est, X, y, cv=cv_a, return_train_score=True)
-        
+        rs = model_selection.ShuffleSplit(n_splits=cv, test_size=0.2)
+        oo = model_selection.cross_validate(est, X, y, cv=rs, return_train_score=None)
         test_score = np.mean(oo['test_score'])
-        train_score = np.mean(oo['train_score'])
-    return est, train_score, test_score
+
+    return test_score
 
 
-def grid_search_single_gene(X: np.ndarray, y: np.ndarray, param: dict, permts: dict, cv: int = 4, **specs):
+def grid_search_single_gene(X: np.ndarray, y: np.ndarray, param: dict, permts: dict, **specs):
     """ evaluate all the permutations for one gene,  """
-    fits = []
-    trainscores = []
+    #- store the data of all permutation
     testscores = []
     for permt in permts:  
         param_a = {**param, **permt}
-        fit, trainscore, testscore = evaluate_single(X, y, param_a, cv,**specs)
-        fits.append(fit)
-        trainscores.append(trainscore)
+        testscore = evaluate_single(X, y, param_a,**specs)
         testscores.append(testscore)
-    # find the best candidate. Max score is considered best score. 
-    best_testscore = max(testscores)
-    index = testscores.index(best_testscore)
-    best_param = permts[index]
-    best_est = fits[index]
-    best_trainscore = trainscores[index]
+    # find the best candidate amongst different permutation. Max score is considered best score. 
+    # best_testscore = max(testscores)
+    # index = testscores.index(best_testscore)
+    # best_param = permts[index]
+    # best_est = fits[index]
+    # best_trainscore = trainscores[index]
 
-    return best_trainscore, best_testscore, best_param, best_est
+    # return best_trainscore, best_testscore, best_param, best_est
+    return testscores
 
 
 def grid_search_single_permut(Xs, ys, param: dict, permt: dict, cv: int = 4, **specs):
     """ evalute all genes for one permutation of param """
     param_a = {**param, **permt}
-    fits = []
-    trainscores = []
     testscores = []
     for X, y in zip(Xs, ys):
-        fit, trainscore, testscore = evaluate_single(X, y, param_a, cv,**specs)
-        fits.append(fit)
-        trainscores.append(trainscore)
+        testscore = evaluate_single(X, y, param_a,**specs)
         testscores.append(testscore)
-    return trainscores, testscores, fits
+    return testscores
 
 
 def map_gene(args):
@@ -87,6 +78,7 @@ def map_gene(args):
     X_train, _, y_train, _ = data[i]
     args['X'] = X_train
     args['y'] = y_train
+    del args['data']
     args_rest = {key: value for key, value in args.items() if key != 'i'}
     return i, grid_search_single_gene(**args_rest)
 
@@ -99,26 +91,19 @@ def map_permut(args):
     return i, grid_search_single_permut(**args_rest)
 
 
-def search(data: Data, param, permts, n_jobs, output_dir=None, **specs):
-    """Evaluates the permts and returns the best results for each gene """
+def run(data: Data, param: dict, permts:list, n_jobs:int, **specs):
+    """Evaluates all permts for each genes"""
     time_start = time.time()
     if 'n_jobs' in param: 
         del param['n_jobs']
     n_genes = len(data)
-
     # Run the search
-    best_testscores = [None for _ in range(n_genes)]
-    best_trainscores = [None for _ in range(n_genes)]
-    best_params = [None for _ in range(n_genes)]
-    best_ests = [None for _ in range(n_genes)]
+    testscores = [None for _ in range(n_genes)] # for all nodes
     if n_jobs == 1:  # serial
         for i in range(n_genes):
             output = map_gene({'i': i, 'data': data, 'param': param, 'permts': permts, **specs})
-            best_trainscore, best_testscore, best_param, best_est = output[1]
-            best_testscores[i] = best_testscore
-            best_trainscores[i] = best_trainscore
-            best_params[i] = best_param
-            best_ests[i] = best_est
+            testscores_node = output[1]
+            testscores[i] = testscores_node
     else:  # parallel
         # multithreading happens either on gene_n or permuts, depending which one is bigger
         pool = Pool(n_jobs)
@@ -128,11 +113,8 @@ def search(data: Data, param, permts, n_jobs, output_dir=None, **specs):
             all_output = pool.map(map_gene, map_input)
             all_output.sort(key=lambda x: x[0])
             for i_gene, output in enumerate(all_output):  # an output for a gene
-                best_trainscore, best_testscore, best_param, best_est = output[1]
-                best_testscores[i_gene] = best_testscore
-                best_trainscores[i_gene] = best_trainscore
-                best_params[i_gene] = best_param
-                best_ests[i_gene] = best_est
+                testscores_node = output[1]
+                testscores[i_gene] = testscores_node
         else:  # when there is more permuts
             print('Permutation-based multi threading')            
             Xs = []
@@ -144,94 +126,85 @@ def search(data: Data, param, permts, n_jobs, output_dir=None, **specs):
 
             input_data = [{'i': i, 'Xs': Xs, 'ys':ys, 'param': param, 'permt': permts[i], **specs} for i in range(len(permts))]
             all_output = pool.map(map_permut, input_data)
-            # print(all_output)
-            try:
-                all_output.sort(key=lambda x: x[0])
-            except TypeError:
-                print('Error in the outputs of map permut')
-                print(all_output)
-                raise TypeError()
+            
+            all_output.sort(key=lambda x: x[0])
 
+            #- extract results for all permutations for each genes
             testscores = np.empty([n_genes, 0])
-            trainscores = np.empty([n_genes, 0])
-            ests = np.empty([n_genes, 0])
             for output in all_output:  # each output is for a permut
-                trainscores_all_genes, testscores_all_genes, ests_all_genes = output[1]  # for all genes
+                testscores_all_genes = output[1]  # for all genes
                 testscores = np.c_[testscores, testscores_all_genes]
-                trainscores = np.c_[trainscores, trainscores_all_genes]
-                ests = np.c_[ests, ests_all_genes]
-            best_trainscores = list(np.max(trainscores, axis=1))
-            best_testscores = list(np.max(testscores, axis=1))
-            best_indices = np.array(testscores.argmax(1))
-            best_ests = ests[range(n_genes), best_indices]
-            best_params = [permts[i] for i in best_indices]
+            
     time_end = time.time()
     print('Param search is completed in %.3f seconds' % (time_end-time_start))
-    return best_trainscores, best_testscores, best_params, best_ests
+    return testscores
 
 
-def permutation(param_grid, output_dir=None):
+def permutation(param_grid):
     tags = list(param_grid.keys())
     values = list(param_grid.values())
     permts = []
     for value in list(itertools.product(*values)):
         permts.append({tag: i for tag, i in zip(tags, value)})
-    if output_dir is not None:
-        if not os.path.isdir(output_dir):
-            os.makedirs(output_dir)
-        with open(os.path.join(output_dir, 'permts.txt'), 'w') as f:
-            print({'permts': permts}, file=f)
     return permts
 
 
-def grid_search(Xs, ys, param, param_grid, n_jobs=1, **specs):
-    """ 
-    spces -- grid search related params such as CV which is passed to single grid search
-    """
-    # TODO: verify the inputs
-    # if n_jobs are given for param, remove it
-    
-    # permuation of param grid
+def rand_search(data: Data, param, param_grid, n_jobs=1, n_sample=60, random_state=None, i_start=0, i_end=1, output_dir='', **specs):
     # print('Grid params:', param_grid)
     permts = permutation(param_grid)
-    print('stats: %d genes %d permts %d threads' % (Xs[0].shape[1], len(permts), n_jobs))
-    print(f'Running complete samples {len(permts)}')
-    return search(Xs, ys, param, permts=permts, n_jobs=n_jobs, **specs)
-
-
-
-def rand_search(data: Data, param, param_grid, n_jobs=1, n_sample=60, output_dir=None, random_state=None, **specs):
-    # print('Grid params:', param_grid)
-    permts = permutation(param_grid, output_dir)
     print('stats: %d genes %d permts %d threads' % (len(data), len(permts), n_jobs))
     if len(permts) < n_sample:
         sampled_permts = permts
     else:
         random.seed(random_state)
         sampled_permts = random.sample(permts, n_sample)
+    #- create the result dir if it doesnt exist
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    #- give warning if the dir is not empty 
+    if os.listdir(output_dir):
+        print('Output directory is not empty')
+    #- write sampled_permts to the file
+    with open(os.path.join(output_dir, 'sampled_permts.txt'), 'w') as f:
+        print({'sampled_permts': sampled_permts}, file=f)
+    #- run the iteration
+    print(f'Running {len(sampled_permts)} samples randomly from {i_start} to {i_end} iterations')
+    for i in range(i_start, i_end):
+        print(f'----Run iteration {i}-----')
+        testscores = run(data, param, permts=sampled_permts, n_jobs=n_jobs, **specs)
+        print(f'Mean best score: {np.mean(np.max(testscores,axis=1))}')
+        #- write the scores to the file
+        FILE = os.path.join(output_dir, f'data_{i}.txt')
+        np.savetxt(FILE, testscores, delimiter=',')
 
-    sampled_permts_sorted = {key: [item[key] for item in sampled_permts] for key in param_grid.keys()}
+def pool(output_dir, n_repeat):
+    """Pools iterative results 
+    Each data in stack contains node results for all permuts, i.e. scores[i_gene][i_purmut].
+    """
 
-    if output_dir is not None:
-        with open(os.path.join(output_dir, 'sampled_permts.txt'), 'w') as f:
-            print({'permts': sampled_permts}, file=f)
-    print(f'Running {len(sampled_permts)} samples randomly')
+    #- read the data and create pool and average values
+    with open(os.path.join(output_dir, 'sampled_permts.txt')) as f:
+        sampled_permts = eval(f.read())['sampled_permts']
+    scores_stack = [] 
+    for i in range(n_repeat):
+        FILE = os.path.join(output_dir, f'data_{i}.txt')
+        scores = np.genfromtxt(FILE, delimiter=',')
+        scores_stack.append(scores)    
+    print('stack shape: n_repeat*n_genes*n_permut: ',np.array(scores_stack).shape)
+    #- reformat from [n_repeat][i_gene][i_purmut] to [i_gene][i_purmut][n_repeat]
+    scores_pool = np.stack(scores_stack, axis=2)
+    # print(scores_pool[2][0])
+    # print('scores pool shape: n_genes*n_permut*n_repeat: ', scores_pool.shape)
+    #- best params
+    scores_mean = np.mean(scores_pool,axis=2)
+    # print('scores mean shape: n_genes*n_permut: ', scores_mean.shape)
+    best_scores = np.max(scores_mean, axis=1)
+    # print('scores best shape: n_genes: ', best_scores.shape)
+    best_indices = np.argmax(scores_mean, axis=1) # highest scores across mutations
+    best_params= [sampled_permts[i] for i in best_indices]
+    print(f'Best score -> min:{np.min(best_scores)},  average: {np.mean(best_scores)}, std: {np.std(best_scores)}')
+    return best_scores, best_params
 
-    best_trainscores, best_testscores, best_params, best_ests = search(data, param, permts=sampled_permts, n_jobs=n_jobs, output_dir=output_dir, **specs)
-    return best_trainscores, best_testscores, best_params, best_ests, sampled_permts_sorted
 
 
-def rand_search_partial(Xs, ys, param, param_grid, n_genes, n_jobs = 1, n_sample=60, **specs):
-    """Conducts random search on hyper-parameters, only partially on n_genes, randomly selected from all genes"""
-    permts = permutation(param_grid, output_dir)
-    print(f'partial rand search on {n_genes} genes with permts {len(permts)}  threads {n_jobs}')
-    if len(permts) < n_sample:
-        sampled_permts = permts
-    else:
-        sampled_permts = random.sample(permts, n_sample)
-    sampled_permts_sorted = {key: [item[key] for item in sampled_permts] for key in param_grid.keys()}
-    print(f'Running {len(sampled_permts)} samples randomly')
-    # choose n genes randomly from all genes
-    choice = [random.randint(0, len(ys)-1) for i in range(n_genes)]
-    best_scores, best_params, best_ests = search(Xs[choice], ys[choice], param, param_grid, permts=sampled_permts, n_jobs=n_jobs, **specs)
-    return best_scores, best_params, best_ests, sampled_permts_sorted
+
